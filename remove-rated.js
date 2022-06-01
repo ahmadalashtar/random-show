e65b7518d47e5e987597cdd919feafe00f11c7de
ahmadalashtar01 <41813167+ahmadalashtar01@users.noreply.github.com>
2022-06-01 23:33:45 +0300

    Delete remove-rated.js

--- remove-rated.js
+++
@@ -1,7 +0,0 @@
-let x = document.getElementsByName("ur")
-let outerFrame = document.getElementsByClassName("lister-item mode-advanced")
-for (let i = 0 ; i < x.length ; i++) {
-  if (x[i].innerHTML!="Rate this"){
-    outerFrame.item(i).remove()
-  }
-}
